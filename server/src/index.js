ef1d8ce8f0794ab1b7397fd868d905172df52580
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-01-07 16:51:07 +0200

    Add routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import cors from "cors";
+import routes from "./routes.js";
 
 const server = express();
+
+// Setum mongoose
+
+// Add cors
 server.use(cors());
 
+// Add routes
+server.use(routes);
+
 server.get("/", (req, res) => {
-    res.send("Working...")
-})
+    res.send([]);
+});
 
 server.listen(5000, () => console.log("Server is listening on http://localhost:5000..."))
