41cf995530feff703c4a12ac58ea85438710c7dd
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-01-08 22:08:21 +0200

    Add Header styles module and logo to Header component

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,10 +1,21 @@
-import { Link } from "react-router";
+import { Link, NavLink } from "react-router";
+import styles from "./Header.module.css";
 
 export default function Header() {
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/dogs">Dogs</Link>
-        </nav>
+
+        <header className={styles["header"]}>
+            <Link to="/"><img src="../../public/images/Logo.png" alt="logo" className={styles["logo"]} /></Link>
+
+            <div>
+                <nav className={styles["nav-bar"]}>
+                    <NavLink className={({ isActive }) => isActive ? styles["selected"] : styles["link"]} to="/">Home</NavLink>
+                    <NavLink className={({ isActive }) => isActive ? styles["selected"] : styles["link"]} to="/dogs">Dogs</NavLink>
+                </nav>
+            </div>
+
+        </header>
+
+
     );
 }
