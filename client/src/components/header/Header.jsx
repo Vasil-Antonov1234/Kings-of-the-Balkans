97b94dc803370961f1ced30f9194b35bb413e229
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-01-11 23:05:06 +0200

    Add responsive navigation

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,27 +1,37 @@
 import { Link, NavLink } from "react-router";
 import styles from "./Header.module.css";
+import { useState } from "react";
 
 export default function Header() {
-    return (
+    const [smallMenuOpen, setSmalMenuOpen] = useState(false);
 
+    function openMenuHandler() {
+        setSmalMenuOpen(!smallMenuOpen)
+    }
+
+    return (
         <header className={styles["header"]}>
-            <Link to="/" className={styles["image-container"]}><img src="../../public/images/Logo.png" alt="logo" className={styles["logo"]} /></Link>
+            <Link to="/" className={styles["image-container"]}>
+                <img src="../../public/images/Logo.png" alt="logo" className={styles["logo"]} />
+            </Link>
 
             <div className={styles["nav-container"]}>
-                <nav className={styles["nav-bar"]}>
-                    <ul>
-                        <li>
-                            <NavLink className={({ isActive }) => isActive ? styles["selected"] : styles["link"]} to="/">Home</NavLink>
-                        </li>
-                        <li>
-                            <NavLink className={({ isActive }) => isActive ? styles["selected"] : styles["link"]} to="/dogs">Dogs</NavLink>
-                        </li>
-                        <li>
-                            <NavLink className={({ isActive }) => isActive ? styles["selected"] : styles["link"]} to="/about">About</NavLink>
-                        </li>
-                    </ul>
+                <nav className={smallMenuOpen ? styles["open"] : ""}>
+                    <div className={styles["nav-bar"]}>
+                        <ul>
+                            <li>
+                                <NavLink className={({ isActive }) => isActive ? styles["selected"] : styles["link"]} to="/">Home</NavLink>
+                            </li>
+                            <li>
+                                <NavLink className={({ isActive }) => isActive ? styles["selected"] : styles["link"]} to="/dogs">Dogs</NavLink>
+                            </li>
+                            <li>
+                                <NavLink className={({ isActive }) => isActive ? styles["selected"] : styles["link"]} to="/about">About</NavLink>
+                            </li>
+                        </ul>
+                    </div>
                 </nav>
-                <div className={styles["small-nav-bar"]}>
+                <div className={styles["small-nav-bar"]} onClick={openMenuHandler}>
                     <span></span>
                     <span></span>
                     <span></span>
