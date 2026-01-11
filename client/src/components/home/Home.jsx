4e8612caef392d5151f6fe63ca0e6bf7316dc6c7
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-01-11 17:10:35 +0200

    Add background image to Home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
             <div className={styles["title-container"]}>
                 <h2 className={styles.title}>King of the Balkans</h2>
             </div>
-            <img src="https://jirikomarek.net/img/previews/bulgaria-central-balkan-mountains-2018.webp" alt="background" className={styles["background"]} />
+            <img src="../../public/images/stara-planina.webp" alt="background" className={styles["background"]} />
         </div>
     )
 }
