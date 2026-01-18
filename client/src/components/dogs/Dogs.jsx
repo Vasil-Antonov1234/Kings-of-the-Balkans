300006d8575d3d5268f1f6e73fe2cb80fdaaf705
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-01-18 12:37:00 +0200

    Create useFetch custom hook

--- client/src/components/dogs/Dogs.jsx
+++ client/src/components/dogs/Dogs.jsx
@@ -1,27 +1,12 @@
-import { useEffect, useState } from "react";
 import DogCard from "../dog-card/DogCard.jsx";
 import styles from "./Dogs.module.css";
+import useFetch from "../../hooks/useFetch.js";
 
 export default function Dogs() {
-    const [dogs, setDogs] = useState([]);
+    const { data } = useFetch("/data/dogs", [])
 
-    useEffect(() => {
-
-        (async () => {
-
-            try {
-                const response = await fetch("http://localhost:5000/data/dogs");
-                const result = await response.json();
-
-                setDogs(result);
-            } catch (error) {
-                alert(error.message);
-            };
-
-        })();
-
-    }, []);
-
+    const dogs = data;
+    
     const males = dogs.filter((dog) => dog.gender === "Male");
     const females = dogs.filter((dog) => dog.gender === "Female");
     const puppies = dogs.filter((dog) => dog.gender === "Puppy");
---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:5000"
+
+export default function useFetch(url, initialState) {
+
+    const [data, setData] = useState(initialState);
+    const [isPending, setIsPending] = useState(true);
+
+    useEffect(() => {
+
+        if (!url || url.includes("undefined")) {
+            return;
+        };
+
+        const abordController = new AbortController();
+
+        (async () => {
+            try {
+                const response = await fetch(`${BASE_URL}${url}`, { signal: abordController.signal });
+
+                if (!response.ok) {
+                    setIsPending(false)
+                    return {};
+                };
+
+                const result = await response.json();
+
+                setData(result);
+                setIsPending(false);
+            } catch (error) {
+
+                if (error.message !== "AbortError" && error.message !== "signal is aborted without reason") {
+
+                    if (error.message === "Failed to fetch") {
+                        return alert("Server is unavailable");
+
+                    };
+
+                    alert(error.message)
+
+                }
+
+            }
+        })();
+
+        return () => {
+            abordController.abort();
+        }
+
+    }, [url]);
+
+    async function request(url, method = "GET", body) {
+        const options = { method: method };
+
+        if (body) {
+            options.headers = { "content-type": "application/json" };
+            options.body = JSON.stringify(body);
+        };
+
+        try {
+            const response = await fetch(`${BASE_URL}${url}`, options);
+
+            if (!response.ok) {
+                const result = await response.json();
+                throw result;
+            }
+
+            const result = await response.json();
+
+            setData(result);
+            setIsPending(false);
+
+            return result;
+        } catch (error) {
+            alert(error.message);
+        };
+
+    }
+
+    return { data, setData, isPending, request };
+};
