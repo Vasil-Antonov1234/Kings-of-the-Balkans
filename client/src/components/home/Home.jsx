a4dc9a4639eaed8e1a5e4e7306e39a142697d9ac
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-01-20 00:52:30 +0200

    Update Home.mudule.css and DogCard.module.css

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -6,8 +6,12 @@ export default function Home() {
             <div className={styles["title-container"]}>
                 <h1 className={styles.title}>Kings of the Balkans</h1>
                 <p className={styles["sub-text"]}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et delectus qui omnis iste, incidunt iusto?</p>
+                <section className={styles["images-container"]}>
+                    <img src="https://www.akc.org/wp-content/uploads/2017/11/Doberman-Pinscher-standing-outdoors.jpg" alt="image-dog-one" />
+                    <img src="https://www.akc.org/wp-content/uploads/2017/11/Doberman-Pinscher-standing-outdoors.jpg" alt="image-dog-one" />
+                    <img src="https://www.akc.org/wp-content/uploads/2017/11/Doberman-Pinscher-standing-outdoors.jpg" alt="image-dog-one" />
+                </section>
             </div>
-            {/* <img src="../../public/images/stara-planina.webp" alt="background" className={styles["background"]} /> */}
         </div>
     )
 }
