d53f716a98ac66d5f839f771726b1a84df1bcc65
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-01-20 00:02:12 +0200

    Update Footer.module.css

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -8,43 +8,47 @@ import styles from "./Footer.module.css"
 export default function Footer() {
     return (
         <footer>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis est cupiditate qui dolorem. Nisi, error, eveniet maiores eligendi molestias incidunt facere explicabo cupiditate nemo obcaecati laudantium sapiente illum, reiciendis voluptas?</p>
-            <div className={styles["address-wrapper"]}>
-                <div className={styles["phone"]}>
-                    <h5>Phone:</h5>
-                    <span>+359 123 456 789</span>
+            <section className={styles["text-logo-container"]}>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis est cupiditate qui dolorem. Nisi, error, eveniet maiores eligendi molestias incidunt facere explicabo cupiditate nemo obcaecati laudantium sapiente illum, reiciendis voluptas?</p>
+                <div className={styles["logo-wrapper"]}>
+                    <img src="../../../public/images/Logo.png" alt="logo" className={styles["logo"]} />
+                    <img src="../../../public/images/FCI_logo.svg.png" alt="logo-FCI" className={styles["logo-fci"]} />
                 </div>
-                <div className={styles["street"]}>
-                    <h5>Address:</h5>
-                    <span>San Francisco 49ers, Mcallister Street, California(CA)</span>
+            </section>
+            <section className={styles["address-nav-container"]}>
+                <div className={styles["address-wrapper"]}>
+                    <div className={styles["phone"]}>
+                        <h5>Phone:</h5>
+                        <span>+359 123 456 789</span>
+                    </div>
+                    <div className={styles["street"]}>
+                        <h5>Address:</h5>
+                        <span>San Francisco 49ers, Mcallister Street, California(CA)</span>
+                    </div>
+                    <div className={styles["icon-wrapper"]}>
+                        <Link>
+                            <FontAwesomeIcon icon={faSquareFacebook} className={styles["icon"]} />
+                        </Link>
+                        <Link>
+                            <FontAwesomeIcon icon={faEnvelope} className={styles["icon"]} />
+                        </Link>
+                    </div>
                 </div>
-                <div className={styles["icon-wrapper"]}>
-                    <Link>
-                        <FontAwesomeIcon icon={faSquareFacebook} className={styles["icon"]} />
-                    </Link>
-                    <Link>
-                        <FontAwesomeIcon icon={faEnvelope} className={styles["icon"]} />
-                    </Link>
-                </div>
-            </div>
-            <div className={styles["logo-wrapper"]}>
-                <img src="../../../public/images/Logo.png" alt="logo" className={styles["logo"]} />
-                <img src="../../../public/images/FCI_logo.svg.png" alt="logo-FCI" className={styles["logo-fci"]} />
-            </div>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/story">Story</Link>
-                </li>
-                <li>
-                    <Link to="/dogs">Dogs</Link>
-                </li>
-                <li>
-                    <Link to="/contacts">Contacts</Link>
-                </li>
-            </ul>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/story">Story</Link>
+                    </li>
+                    <li>
+                        <Link to="/dogs">Dogs</Link>
+                    </li>
+                    <li>
+                        <Link to="/contacts">Contacts</Link>
+                    </li>
+                </ul>
+            </section>
         </footer>
     )
 }
