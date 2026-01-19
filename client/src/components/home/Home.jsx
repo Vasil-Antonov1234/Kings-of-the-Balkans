20c53f23ddc47d98d4b7340f53921e0ddde94ee6
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-01-20 00:14:44 +0200

    Update Home.module.css

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -5,6 +5,7 @@ export default function Home() {
         <div className={styles["home-container"]}>
             <div className={styles["title-container"]}>
                 <h1 className={styles.title}>Kings of the Balkans</h1>
+                <p className={styles["sub-text"]}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et delectus qui omnis iste, incidunt iusto?</p>
             </div>
             {/* <img src="../../public/images/stara-planina.webp" alt="background" className={styles["background"]} /> */}
         </div>
