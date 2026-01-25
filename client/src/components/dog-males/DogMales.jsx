3665de19879a328b8d42a4bc94f19b4ba96db69e
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-01-25 21:02:01 +0200

    Update DogMales component and DogMales.module.css

--- client/src/components/dog-males/DogMales.jsx
+++ client/src/components/dog-males/DogMales.jsx
@@ -6,10 +6,16 @@ export default function DogMales() {
     const { data, isPending } = useFetch("/dogs/males", []);
 
     return (
-        <section className={styles["main-container"]}>
-            {isPending ? <h2 className={styles["loading"]}>Loading...</h2> :
-                data.length ? data.map((dog) => <DogCard key={dog._id} dog={dog} />) : <h2 className={styles["message"]}>We currently have no male dogs, please come back again.</h2>
-            }
+        <section className={styles["wrapper"]}>
+            <section>
+                <div className={styles["line"]}></div>
+                <section className={styles["main-container"]}>
+                    {isPending ? <h2 className={styles["loading"]}>Loading...</h2> :
+                        data.length ? data.map((dog) => <DogCard key={dog._id} dog={dog} />) : <h2 className={styles["message"]}>We currently have no male dogs, please come back again.</h2>
+                    }
+                </section>
+            </section>
+            <div className={styles["line"]}></div>
         </section>
     );
 }
