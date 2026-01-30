2735f0e7f0144162102afc9e9df9e76727e010eb
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-01-30 11:17:54 +0200

    Refactor Upload Picture component to work only for upload functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Footer from "./components/footer/Footer.jsx";
 import DogDetails from "./components/dogs/dog-details/DogDetails.jsx";
 import Details from "./components/details/Details.jsx";
 import UploadPicture from "./components/upload-picture/UploadPicture.jsx";
+import UploadPicture1 from "./components/upload-picture/UploadPicture1.jsx";
 
 function App() {
 
@@ -23,7 +24,7 @@ function App() {
         <Route path="/dogs/females" element={<DogDetails dogs={"females"} />}/>
         <Route path="/dogs/puppies" element={<DogDetails dogs={"puppies"}/>}/>
         <Route path="/dogs/:dogId/details" element={<Details />} />
-        <Route path="/pictures/upload-picture" element={<UploadPicture />} />
+        <Route path="/pictures/upload-picture" element={<UploadPicture1 />} />
         <Route path="/pictures/:dogId/upload-picture" element={<UploadPicture />} />
       </Routes>
 
---
+++ client/src/components/upload-picture/UploadPicture1.jsx
@@ -0,0 +1,64 @@
+import useFetch from "../../hooks/useFetch.js";
+import useForm from "../../hooks/useForm.js";
+import styles from "./UploadPicture.module.css"
+
+const initialValues = {
+    pictureName: "",
+    pictureUrl: ""
+}
+
+export default function UploadPicture1() {
+
+    const { formHandler, formInputRegister } = useForm(initialValues, managePictureInputHandler);
+    const { request } = useFetch();
+
+    async function managePictureInputHandler(values) {
+
+        if (!values.pictureName) {
+            return alert("Picture name is required!");
+        };
+
+        if (!values.pictureUrl) {
+            return alert("Picture url is required!");
+        };
+
+        try {
+            await request(`/pictures/upload`, "POST", values);
+        } catch (error) {
+            alert(error)
+        }
+
+    }
+
+    return (
+        <section className={styles["main-ctr"]}>
+            <div className={styles["wrapper"]}>
+                <h1>Upload picture</h1>
+                <form action={formHandler}>
+                    {/* Name */}
+                    <div className={styles["name-ctr"]}>
+                        <input
+                            {...formInputRegister("pictureName")}
+                            type="text"
+                            placeholder="Picture name"
+                        />
+                    </div>
+
+                    {/* Picture url */}
+                    <div className={styles["url-ctr"]}>
+                        <input
+                            {...formInputRegister("pictureUrl")}
+                            type="text"
+                            placeholder="Picture url"
+                        />
+                    </div>
+
+                    {/* Upload button */}
+                    <div className={styles["submit-btn-ctr"]}>
+                        <button className={styles["submit-btn"]}>Upload</button>
+                    </div>
+                </form>
+            </div>
+        </section>
+    );
+}
