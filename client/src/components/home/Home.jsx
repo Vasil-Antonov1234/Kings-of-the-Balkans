f8f774000e8387c82a8c9ace6f49895e7b2bb8a7
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-02-08 23:26:16 +0200

    Update Home cmponent and Home.module.css

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,4 +1,5 @@
 import styles from "./Home.module.css";
+import { Link } from "react-router"
 
 export default function Home() {
     return (
@@ -6,12 +7,21 @@ export default function Home() {
             <div className={styles["title-container"]}>
                 <h1 className={styles.title}>Kings of the Balkans</h1>
                 <p className={styles["sub-text"]}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et delectus qui omnis iste, incidunt iusto?</p>
-                <section className={styles["images-container"]}>
-                    <img src="https://www.akc.org/wp-content/uploads/2017/11/Doberman-Pinscher-standing-outdoors.jpg" alt="image-dog-one" />
-                    <img src="https://www.akc.org/wp-content/uploads/2017/11/Doberman-Pinscher-standing-outdoors.jpg" alt="image-dog-one" />
-                    <img src="https://www.akc.org/wp-content/uploads/2017/11/Doberman-Pinscher-standing-outdoors.jpg" alt="image-dog-one" />
-                </section>
             </div>
+            <section className={styles["images-container"]}>
+                <Link className={styles["wrapper"]} to="/dogs">
+                    <img src="https://media.istockphoto.com/id/901452286/photo/three-doberman-pincher-relaxing-in-the-park.jpg?s=612x612&w=0&k=20&c=82UiXd_KuHtCSg_WgxT-jolgzNn_8FstJ97-GMQ4dQE=" alt="image-dog-one" />
+                    <span>OUR DOGS</span>
+                </Link>
+                <Link className={styles["wrapper"]} to="/story">
+                    <img src="https://www.shutterstock.com/image-photo/three-doberman-runaway-camera-back-260nw-2497401555.jpg" alt="image-dog-one" />
+                    <span>STORY</span>
+                </Link>
+                <Link className={styles["wrapper"]} to="/contacts">
+                    <img src="https://www.shutterstock.com/image-photo/using-laptop-show-icon-address-600nw-2521386695.jpg" alt="image-dog-one" />
+                    <span>CONTACTS</span>
+                </Link>
+            </section>
         </div>
     )
 }
