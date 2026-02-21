10e2fa9e594f0aa802c15213983bae6afd0dca8d
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-02-21 22:29:08 +0200

    Update DogCard component and DogCard.css module

--- client/src/components/dogs/dog-card/DogCard.jsx
+++ client/src/components/dogs/dog-card/DogCard.jsx
@@ -5,14 +5,16 @@ export default function DogCard({
     dog
 }) {
     return (
-        <Link to={`/dogs/${dog._id}/details`} className={styles["link"]}>
+        <article to={`/dogs/${dog._id}/details`} className={styles["wrapper"]}>
             <article className={styles["article"]}>
                 <h3>{dog.name}</h3>
                 <div className={styles["img-container"]}>
                     <img src={dog.imageUrl} alt="dog image" className={styles["image"]} />
                 </div>
             </article>
-            <span className={styles["details"]}>Show more</span>
-        </Link>
+            <Link to={`/dogs/${dog._id}/details`}>
+                <span className={styles["details"]}>Show more</span>
+            </Link>
+        </article>
     );
 }
