47c4889b1cfe149a90e6c189353a81c4e65c8e7f
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-04-20 12:41:09 +0300

    Update authMiddleware.js
    Update authMiddleware to throw an error and cancel the request in case the access token is in the invalid tokens collection

--- client/functions/src/middlewares/authMiddleware.js
+++ client/functions/src/middlewares/authMiddleware.js
@@ -1,14 +1,31 @@
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config/constants.js";
+import Token from "../models/Token.js";
+import authService from "../services/authService.js";
 
-export default function authMiddleware(req, res, next) {
+export default async function authMiddleware(req, res, next) {
     const accessToken = req.get("X-Authorization");
 
     try {
+        // Check if the user has accessToken
         if (!accessToken) {
             throw new Error("Unauthorized!");
         };
 
+        // Check if the user is logged out and the accessToken is no longer valid
+        const expired = await authService.getExpiredTokens();
+
+        const expiredTokens = expired.tokens;
+
+        const onlyTokens = [];
+
+        expiredTokens.forEach((tokenTime) => onlyTokens.push(tokenTime[0]));
+
+        if (onlyTokens.includes(accessToken)) {
+            throw new Error("Unauthorized!");
+        };
+
+        // Verify the accessToken
         const decodedToken = jwt.verify(accessToken, JWT_SECRET);
 
         // Attach authenticated user to request
