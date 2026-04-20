f22aa85ef28395ff7a41689b1481b56fe9414f2a
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-04-20 12:37:02 +0300

    Update authService.js
    Added logout method to authServoce which adds the access token to the collection of invalid tokens if this token is not already added and delete all access tokens which expiration time is already expired.
    Added getExpiredTokens method

--- client/functions/src/services/authService.js
+++ client/functions/src/services/authService.js
@@ -2,19 +2,20 @@ import User from "../models/User.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config/constants.js";
+import Token from "../models/Token.js";
 
 const authService = {
     async register(userData) {
-        
+
         return User.create(userData);
 
     },
     async login(userData) {
         const { username, password } = userData;
-        
+
         // Validate user
         const user = await User.findOne({ username });
-        
+
         if (!user) {
             throw new Error("Wrong username or password!");
         };
@@ -35,6 +36,34 @@ const authService = {
         const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
 
         return token;
+    },
+    async logout(accessToken) {
+
+        const expired = await Token.findOne({ type: "expired" });
+        const expiredTokens = expired.tokens;
+        const expirationTime = 3600000;
+        const now = new Date().getTime();
+
+        const updatedTokens = expiredTokens.filter((tokenTime) => (now - tokenTime[1]) < expirationTime);
+
+        const onlyTokens = [];
+
+        updatedTokens.forEach((tokenTime) => onlyTokens.push(tokenTime[0]));
+
+        if (!onlyTokens.includes(accessToken)) {
+            updatedTokens.push([accessToken, now]);
+        }
+
+
+        const update = { tokens: updatedTokens };
+
+        await Token.findOneAndUpdate({ type: "expired" }, update);
+
+    },
+    async getExpiredTokens() {
+        const tokens = await Token.findOne({ type: "expired" });
+
+        return tokens;
     }
 }
 
